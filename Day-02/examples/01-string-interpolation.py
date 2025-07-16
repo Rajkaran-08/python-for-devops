a09924a5689dee3a5ab626cf6ca138286f416f36
Rajkaran-08 <srajkaran08@gmail.com>
2025-07-16 12:21:19 -0400

    Created String Interpolation

---
+++ Day-02/examples/01-string-interpolation.py
@@ -0,0 +1,12 @@
+name = "Raj"
+
+#Using f-strings
+print(f"Hello, {name.title()}")
+
+#Using modulas (this is an older method)
+print("Hi %s" % name)
+
+#Using .format()
+print("Hola, {}." .format(name))
+#Here .fromat() is indexed
+print("The {0} is faster than the {1}.".format("cheetah", "elephant")) 
